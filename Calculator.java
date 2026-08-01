54b0dfa895a4a9808d96d5e37bfa22ac00200df6
Gairika <dasgairika416@gmail.com>
2026-08-01 09:35:10 -0700

    Day 1 Java programs

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public static void main(String[] args) 
+    {
+        int a = 20;
+        int b = 10;
+
+        System.out.println("Addittion = " + (a+b));
+        System.out.println("Subtraction = " + (a-b));
+        System.out.println("Multiplication = " + (a*b));
+        System.out.println("Addittion = " + (a/b));
+    }
+    
+}
---
+++ Rectangle.java
@@ -0,0 +1,12 @@
+public class Rectangle {
+    public static void main(String[] args)
+    {
+        int length = 15;
+        int breadth = 8;
+
+        int area = length * breadth;
+
+        System.out.println("Area of Rectangle = " + area);
+    }
+    
+}
